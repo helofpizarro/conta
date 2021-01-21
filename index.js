73cae7558797e5a65c3d06b18c157e22aabc7b12
Maria Heloisa <heloisa.fpizarro@gmail.com>
2021-01-20 21:41:55 -0300

    inserindo set e get

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente ()
 cliente1.nome = 'Ricardo'
 cliente1.cpf = 111122233309
 
-const cliente2 = new Cliente ()
-cliente2.nome = 'Alice'
-cliente2.cpf = 88822233309
+//const cliente2 = new Cliente ()
+//cliente2.nome = 'Alice'
+//cliente2.cpf = 88822233309
 
 const contaCorrenteRicardo = new ContaCorrente()
 contaCorrenteRicardo.agencia = 1001
@@ -15,7 +15,9 @@ contaCorrenteRicardo.cliente = cliente1
 contaCorrenteRicardo.depositar(500)
 
 const conta2 = new ContaCorrente()
-conta2.cliente = cliente2
+conta2.cliente = cliente1
+conta2.cliente.nome = 'Alice'
+conta2.clinete.cpf = 88822233309
 conta2.agenia = 102
 
 contaCorrenteRicardo.transferir(200,conta2)
